d3d7a9e45e7472f55c40830417cd521d7925aa0b
lmascelli <lmascelli@gmail.com>
2023-12-27 15:56:08 +0100

    h5analog reads info_channels dataset

--- conandeps_legacy.cmake
+++
@@ -1,6 +0,0 @@
-message(STATUS "Conan: Using CMakeDeps conandeps_legacy.cmake aggregator via include()")
-message(STATUS "Conan: It is recommended to use explicit find_package() per dependency instead")
-
-find_package(HDF5)
-
-set(CONANDEPS_LEGACY  HDF5::HDF5 )
